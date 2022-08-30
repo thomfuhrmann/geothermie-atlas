334b3c7227c80ca6daabf50409c70c9095b2d2fd
Thomas Fuhrmann <thomas.fuhrmann@geologie.ac.at>
2022-08-30 10:42:25 +0200

    Changed response

--- pythonShell.js
+++ pythonShell.js
@@ -13,7 +13,7 @@ const runPythonShell = (res, options) => {
         .split(",")
         .map((entry) => entry.trim().replaceAll("'", ""));
     }
-    res.json("Hallo");
+    res.json(results);
   });
 };
 
