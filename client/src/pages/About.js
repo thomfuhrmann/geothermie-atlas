d33091c6cfcb4a1a1e94bcc375e564ff897cd83f
Thomas Fuhrmann <thomas.fuhrmann@geologie.ac.at>
2022-11-21 09:39:31 +0100

    About updated.

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -13,12 +13,12 @@ const About = () => {
       <Content>
         <Heading>Informationen über diese Applikation</Heading>
         <p>
-          Diese Applikation ist als Unterstützung bei der Planung
-          oberflächennaher geothermischer Anlagen gedacht. Es können
-          geothermisch relevante Parameter, und mögliche rechtliche
-          Einschränkungen und Hinweise abgefragt werden. Außerdem können
-          standortspezifisch Berechnungen in Bezug auf die gewünschte
-          Konfiguration der geothermischen Anlage durchgeführt werden.
+          Diese Applikation unterstützt die Planung oberflächennaher
+          geothermischer Anlagen. Es können geothermisch relevante Parameter,
+          und mögliche rechtliche Einschränkungen und Hinweise abgefragt werden.
+          Außerdem können standortspezifisch Berechnungen in Bezug auf die
+          gewünschte Dimensionierung der geothermischen Anlage durchgeführt
+          werden.
         </p>
         <p>
           Die der Applikation zugrunde liegenden Daten wurden im Rahmen des
@@ -31,7 +31,7 @@ const About = () => {
           Dauersiedlungsraum Salzburg und ausgewählte Gebiete in der Steiermark.
           Diese Applikation beschränkt sich ausschließlich auf Wien.
         </p>
-        <Heading>Zusatzinfo zur Berechnungsweise für Erdwäremsonden</Heading>
+        <Heading>Zusatzinfo über interaktive Sondenfeldberechnung </Heading>
         <p>
           Das Programm berechnet die mögliche Leistung und Energie, die aus dem
           vorgegebenen Sondenfeld gewonnen werden kann. Dabei wird die Geometrie
@@ -45,19 +45,18 @@ const About = () => {
           Die Betriebsweise wird vereinfacht in vier Phasen pro Jahr unterteilt:
           Heizbetrieb – Stillstand – Kühlbetrieb - Stillstand.
         </p>
-
         <MediumHeading>Betriebsweise</MediumHeading>
         <p>
-          Die Betriebsfunktion kann durch den Benutzer durch Angabe der
-          gebäudeseitigen Heiz- und Kühlleistung sowie der Jahresbetriebsstunden
-          für Heizen und Kühlen vorgegeben werden. In diesem Fall wird die
-          erzielbare Sondenleistung für das benutzerdefinierte Sondenfeld so
-          ermittelt, dass das Leistungsverhältnis zwischen Heizen und Kühlen
-          eingehalten wird. Es kann auch ein reiner Heiz- bzw. Kühlbetrieb
-          vorgegeben werden. Bei der benutzerdefinierten Betriebsweise wird auch
-          ein Deckungsgrad berechnet, also wieviel Prozent das vorgegebene
-          Sondenfeld von der vorgegebenen Betriebsfunktion abdecken kann. Wird
-          die Betriebsfunktion nicht durch den Benutzer vorgegeben, so wird mit
+          Die Betriebsfunktion kann durch Angabe der gebäudeseitigen Heiz- und
+          Kühlleistung sowie der Jahresbetriebsstunden für Heizen und Kühlen
+          vorgegeben werden. In diesem Fall wird die erzielbare Sondenleistung
+          für das benutzerdefinierte Sondenfeld so ermittelt, dass das
+          Leistungsverhältnis zwischen Heizen und Kühlen eingehalten wird. Es
+          kann auch ein reiner Heiz- bzw. Kühlbetrieb vorgegeben werden. Bei der
+          benutzerdefinierten Betriebsweise wird auch ein Deckungsgrad
+          berechnet, der angibt wieviel Prozent des angegebenen Bedarfs durch
+          das vorgegebene Sondenfeld abgedeckt werden kann. Wird die
+          Betriebsfunktion nicht durch den Benutzer vorgegeben, so wird mit
           einem Normbetrieb gerechnet. Dabei werden die Jahresbetriebsstunden
           für Heizen und Kühlen aus der standortbezogenen Bodentemperatur für
           ein typisches Wohnhaus herangezogen. Die mittlere
@@ -78,23 +77,23 @@ const About = () => {
           mittlere Untergrundtemperatur von 0 bis 100 m Tiefe werden aus den
           Ressourcenkarten für das ausgewählte Grundstück übernommen und fließen
           in die Berechnung ein. Ist die Vorgabe der Sondentiefe größer als 100
-          m, so wird die Untergrundtemperatur mit einem Gradienten von 0.03 K/m
-          mit der Tiefe erhöht. Zusätzlich wird eine Grafik mit der Entwicklung
-          der mittleren Fluidtemperatur in Zusammenhang mit der berechneten
-          Betriebsfunktion ausgegeben.
+          m, so wird die Untergrundtemperatur mit einem Gradienten von 0.03 °C
+          pro Meter mit der Tiefe erhöht. Zusätzlich wird eine Grafik mit der
+          Entwicklung der mittleren Fluidtemperatur in Zusammenhang mit der
+          berechneten Betriebsfunktion ausgegeben.
         </p>
-        <MediumHeading>Parameter für die Berechnung</MediumHeading>
+        <MediumHeading>Zusätzliche Parameter für die Berechnung</MediumHeading>
         <p>
           Die folgenden Parameter werden für alle Simulationen verwendet und
-          können vom Benutzer nicht verändert werden. <br></br>
+          können nicht durch eine Eingabe verändert werden. <br></br>
           <br></br>
           Simulationsjahre: 20 Jahre<br></br>
           Volumetrische Wärmekapazität des Erdreichs: 2.2 MJ/m³/K<br></br>
           Sondenkopf Überdeckung: 1 m <br></br>
           Bohrradius: 0.075 m <br></br>
-          Sondentyp: Duplex 32 mm, 0.04 m <br></br>
-          Rohrabstand Wärmeträgermedium: Ethanol 12 % <br></br>
-          Massenstrom pro Sonde: 0.4 kg /s<br></br>
+          Sondentyp: Duplex 32 mm, 0.04 m Rohrabstand<br></br>
+          Wärmeträgermedium: Ethanol 12 % <br></br>
+          Massenstrom pro Sonde: 0.4 kg/s<br></br>
           Wärmeleitfähigkeit der Verpressung: 2 W/m/K
         </p>
         <MediumHeading>Grenztemperaturen</MediumHeading>
@@ -107,6 +106,59 @@ const About = () => {
           Das Sondenfeld wird im Heizbetrieb auf die minimale Grenztemperatur
           ausgelegt, im Kühlbetrieb auf die maximale Grenztemperatur.
         </p>
+        <MediumHeading>Leistungszahlen</MediumHeading>
+        <p>
+          Folgende Leistungszahlen der Wärmepumpe im Heiz- und Kühlbetrieb
+          werden berücksichtigt:
+        </p>
+
+        <dl>
+          <dt>COP</dt>
+          <dd>
+            Leistungszahl der Wärmepumpe im Heizbetrieb (Coefficient of
+            Performance): Die Leistungszahl für Heizen (COP) wird soleseitig
+            immer auf die untere Grenztemperatur der Erdwärmesonden (Vorlauf -3
+            / Rücklauf 0 °C) ausgelegt. Wasserseitig wird zur Berechnung des COP
+            die vorgegebene Heizungs-Vorlauftemperatur verwendet, welche ohne
+            Nutzereingabe auf 35 °C voreingestellt ist. Diese Leistungszahl gilt
+            also für den Extremfall, wenn die Fluidtemperatur der Sonden den
+            unteren Grenzwert erreicht haben (in der Regel am Ende der
+            Heizsaison nach 20 Betriebsjahren).
+          </dd>
+
+          <dt>JAZ</dt>
+          <dd>
+            Jahresarbeitszahl oder saisonale Leistungszahl der Wärmepumpe im
+            Heizbetrieb: Die Berechnung der Jahresarbeitszahl im Heizbetrieb
+            (JAZ) berücksichtigt die Sole-Fluidtemperaturen jeder Betriebsstunde
+            im Jahr, berechnet den COP und bildet daraus den Mittelwert über
+            alle Betriebsstunden.
+          </dd>
+
+          <dt>EER</dt>
+          <dd>
+            Leistungszahl der Wärmepumpe im Kühlbetrieb (Energy Efficiency
+            Rating): Die Leistungszahl für Kühlen (EER) ist statisch auf eine
+            Fluidtemperatur von 18 °C auf der kalten Seite (gebäudeseitig) und
+            30 °C auf der warmen Seite ausgelegt (erdseitig).
+          </dd>
+
+          <dt>SEER</dt>
+          <dd>
+            Saisonale Leistungszahl der Wärmepumpe im Kühlbetrieb (Seasonal
+            Energy Efficiency Rating): Die Berechnung der saisonalen
+            Leistungszahl im Kühlbetrieb (SEER) berücksichtigt die berechneten
+            Fluid-Temperaturen aller Kühlbetriebsstunden, insbesondere werden
+            Fluidtemperaturen unterhalb 18 °C für die freie Kühlung ohne
+            Wärmepumpe berücksichtigt. Ohne Wärmepumpe ist die Kühlung besonders
+            effizient, daher wird für die Betriebsstunden mit freier Kühlung
+            pauschal ein EER-Wert von 20 angenommen. Die Berechnungsformel
+            basiert auf Messwerte verschiedener Betriebspunkte einer realen
+            Sole-Wasser Wärmepumpe in der Klimakammer und wurde im FFG Projekt
+            ZWEIFELDSPEICHER ermittelt.
+          </dd>
+        </dl>
+
         <Heading>Verwendete 3rd-Party Software</Heading>
         <p>
           Die Berechnungen für Erdwärmesonden werden mit dem Python-Modul{" "}
@@ -130,88 +182,7 @@ const About = () => {
           OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
         </p>
         <p>Copyright (c) 2017-2022, Massimo Cimmino All rights reserved.</p>
-        <Heading>Glossar</Heading>
-        <dl>
-          <dt>COP</dt>
-          <dd>
-            Leistungszahl der Wärmepumpe im Heizbetrieb (Coefficient of
-            Performance). Die Leistungszahl für Heizen (COP) wird soleseitig
-            immer auf die untere Grenztemperatur der Erdsonden (Vorlauf -3 /
-            Rücklauf 0 °C) ausgelegt. Wasserseitig wird zur Berechnung des COP
-            die vorgegebene Heizungs-Vorlauftemperatur verwendet, welche ohne
-            Nutzereingabe auf 35 °C voreingestellt ist. Diese Leistungszahl
-            tritt also nur im Extremfall auf, wenn die Fluidtemperatur der
-            Sonden den unteren Grenzwert erreicht haben (in der Regel am Ende
-            der Heizsaison nach 20 Betriebsjahren).
-          </dd>
 
-          <dt>JAZ</dt>
-          <dd>
-            Jahresarbeitszahl oder saisonale Leistungszahl der Wärmepumpe im
-            Heizbetrieb. Die Berechnung der Jahresarbeitszahl im Heizbetrieb
-            (JAZ) berücksichtigt die Sole-Fluidtemperaturen jeder Betriebsstunde
-            im Jahr, berechnet den COP und bildet daraus den Mittelwert über
-            alle Betriebsstunden.
-          </dd>
-
-          <dt>EER</dt>
-          <dd>
-            Leistungszahl der Wärmepumpe im Kühlbetrieb (Energy Efficiency
-            Rating). Die Leistungszahl für Kühlen (EER) ist statisch auf eine
-            Fluidtemperatur von 18 °C auf der kalten Seite (Gebäudeseitig) und
-            30 °C auf der warmen Seite ausgelegt (Erdseitig)
-          </dd>
-
-          <dt>SEER</dt>
-          <dd>
-            Saisonale Leistungszahl der Wärmepumpe im Kühlbetrieb (Seasonal
-            Energy Efficiency Rating).Die Berechnung der saisonalen
-            Leistungszahl im Kühlbetrieb (SEER) berücksichtigt die berechneten
-            Fluid-Temperaturen aller Kühlbetriebsstunden, insbesondere werden
-            Fluidtemperaturen unterhalb 18 °C für die freie Kühlung ohne
-            Wärmepumpe berücksichtigt. Für die Betriebsstunden mir freier
-            Kühlung wird pauschal ein EER-Wert von 20 angenommen. Die
-            Berechnungsformel basiert auf Messwerte verschiedener Betriebspunkte
-            einer realen Sole-Wasser Wärmepumpe in der Klimakammer und wurde im
-            FFG Projekt ZWEIFELDSPEICHER ermittelt.
-          </dd>
-
-          <dt>Wärmeträgermedium</dt>
-          <dd>Ethanol ist umweltneutral und nicht korrosiv.</dd>
-
-          <dt>Ausgeglichene Betriebsweise</dt>
-          <dd>
-            Bei einer ausgeglichenen Betriebsweise des Sondenfelds können durch
-            die jährliche Regeneration größere Leistungen und Energiemengen
-            erzielt werden, als bei einem einseitigen Betrieb. Bei einem
-            Kühlüberhang ist es empfehlenswert, z.b. für zusätzliche Gebäude,
-            mehr Wärme bereitzustellen. Dies kann zum Beispiel im Rahmen eines
-            Anergienetzes umgesetzt werden. Auch die Kombination mit einer
-            Luftwärmepumpe oder mit Solarthermie kann in Erwägung gezogen
-            werden, um eine optimale Nutzung des Sondenfelds zu erreichen.
-          </dd>
-
-          <dt>Anergienetz</dt>
-          <dd>Auch Niedertemperaturnetz oder kaltes Nahwärmenetz.</dd>
-
-          <dt>Sondentyp</dt>
-          <dd>
-            Es gibt grundsätzlich drei Sondentypen: Simplex, Duplex und
-            Koaxialsonden. Bei Simplex wird eine U-Rohrsonde in das Bohrloch
-            eingebraucht, bei Duplex zwei U-Rohrsonden. Als U-Rohre werden
-            üblicherweise PE100 Rohre eingesetzt mit einem Durchmesser von DN32
-            oder DN40. Eine Koaxialsonde ist radialsymmetrisch aufgebaut, wobei
-            das Sondenfluid im Außenrohr und im Innenrohr nach unten bzw. nach
-            oben fließt.
-          </dd>
-
-          <dt>Verpressung</dt>
-          <dd>
-            Nach Einbringung der Sondenrohre wird der verbleibende Ringraum mit
-            einem Spezialmörtel verfüllt. Dieser sollte eine möglichst hohe
-            Wärmeleitfähigkeit aufweisen.
-          </dd>
-        </dl>
         <Heading>Hinweise und Haftungsausschluss</Heading>
         <p>
           Die thematischen Inhalte auf unserem Webportal dienen dazu, einen
